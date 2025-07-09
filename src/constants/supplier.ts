7249aa9c788dbf5274c1e8e692cf1464dfe0c535
Edgar <egblanco.betasoft@gmail.com>
2025-07-08 19:46:18 -0600

    customer_crud

---
+++ src/constants/supplier.ts
@@ -0,0 +1,45 @@
+export const SUPPLIER_TYPE_OPTIONS = [
+  "Aluminum Die Casting",
+  "Zamac Die Casting", 
+  "CNC Machining",
+  "Aluminum Extrusion",
+  "Injection Molding",
+  "Die Stamping",
+  "Tooling"
+] as const;
+
+export const SIZE_OPTIONS = ["Big", "Medium", "Small"] as const;
+
+export const STATE_OPTIONS = [
+  "Yucatan",
+  "DF", 
+  "Aguascalientes",
+  "Baja California",
+  "Baja California Sur",
+  "Campeche",
+  "Coahuila",
+  "Colima",
+  "Chiapas",
+  "Chihuahua",
+  "Durango",
+  "Guanajuato",
+  "Guerrero",
+  "Hidalgo",
+  "Jalisco",
+  "México",
+  "Michoacán",
+  "Morelos",
+  "Nayarit",
+  "Nuevo León",
+  "Oaxaca",
+  "Puebla",
+  "Querétaro",
+  "Quintana Roo",
+  "San Luis Potosí",
+  "Sinaloa",
+  "Sonora",
+  "Tabasco",
+  "Tamaulipas",
+  "Tlaxcala",
+  "Zacatecas"
+] as const; 
